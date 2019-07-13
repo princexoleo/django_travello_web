4151f99d1b99a867eb76a9ddecd17264ec39d14b
maz_leo <xtleon.xl@gmail.com>
2019-07-13 19:27:28 +0600

    object create and passing dynamic data to template

--- travello_project/travello/views.py
+++ travello_project/travello/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
+from .models import Destination
 
 # Create your views here.
 
 def index(request):
-    return render(request,'travello/index.html',{'title':'Travello'})
+    dest1 = Destination() #object 1
+    dest1.name = 'Mumbai'
+    dest1.desc = 'The city that never sleeps!'
+    dest1.img   ='destination_1.jpg'
+    dest1.price = 700
+    #object 2
+    dest2 = Destination()
+    dest2.name = 'Mymensingh'
+    dest2.desc = 'The beautiful amazing city of Bangladesh!'
+    dest2.img   ='destination_2.jpg'
+    dest2.price = 600
+    #object 3
+    dest3 = Destination()
+    dest3.name = 'Dhaka'
+    dest3.desc = 'The city that never sleeps and always busy!'
+    dest3.img   ='destination_3.jpg'
+    dest3.price = 900
+    
+    dests =[dest1, dest2, dest3]
+    return render(request,'travello/index.html',{'dests':dests,'title':'Travello'})
