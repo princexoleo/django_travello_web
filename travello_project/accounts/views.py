74bfe6a08a641abfe29136d341bb70b67d941c00
maz_leo <xtleon.xl@gmail.com>
2019-07-13 21:24:49 +0600

    custom user registration created

--- travello_project/accounts/views.py
+++ travello_project/accounts/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User, auth
 
 # Create your views here.
 def register(request):
-    pass
-    return render(request, 'accounts/register.html')
+    if request.method =='POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        user_name = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        con_password = request.POST['con_password']
+
+        user = User.objects.create_user(username=user_name, password=password, email=email, first_name=first_name, last_name=last_name)
+        user.save()
+        return redirect('/')
+    else:
+        return render(request, 'accounts/register.html')
+    
 
