29fb67c35ed4f98f9a4f6042162e72fb60db89c1
maz_leo <xtleon.xl@gmail.com>
2019-07-15 16:20:33 +0600

    user validation message passing to html

--- travello_project/accounts/views.py
+++ travello_project/accounts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect
+from django.contrib import messages
 from django.contrib.auth.models import User, auth
 
 # Create your views here.
@@ -13,17 +14,18 @@ def register(request):
         #verify user information
         if password == con_password:
             if User.objects.filter(username=user_name).exists():
-                print('username already taken')
+                messages.info(request,'username already taken')
+                return redirect('register')
             elif User.objects.filter(email=email).exists():
-                print('email already exists')
+                messages.info(request,'email already exists')
+                return redirect('register')
             else:
                 user = User.objects.create_user(username=user_name, password=password, email=email, first_name=first_name, last_name=last_name)
                 user.save()
                 print('user created')
         else:
-            print('password not matching ..')
-
-        
+            messages.info(request,'password not matching ')
+            return redirect('register') 
         return redirect('/')
     else:
         return render(request, 'accounts/register.html')
