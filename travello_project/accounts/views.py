da50eede15ff1c6189b5fa69dc03a1fe32e5da10
maz_leo <xtleon.xl@gmail.com>
2019-07-15 16:55:20 +0600

    login features added
    login html added
    login view added
    login verification added
    after registration go login feature added
    login nav added in index html

--- travello_project/accounts/views.py
+++ travello_project/accounts/views.py
@@ -3,6 +3,22 @@ from django.contrib import messages
 from django.contrib.auth.models import User, auth
 
 # Create your views here.
+
+def login(request):
+    if request.method =='POST':
+        user_name = request.POST['username']
+        password = request.POST['password']
+        print(user_name, password)
+        user = auth.authenticate(username=user_name, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Invalid credentials')
+            return redirect('login')
+    else:
+        return render(request, 'accounts/login.html')
+
 def register(request):
     if request.method =='POST':
         first_name = request.POST['first_name']
@@ -23,6 +39,7 @@ def register(request):
                 user = User.objects.create_user(username=user_name, password=password, email=email, first_name=first_name, last_name=last_name)
                 user.save()
                 print('user created')
+                return redirect('login')
         else:
             messages.info(request,'password not matching ')
             return redirect('register') 
